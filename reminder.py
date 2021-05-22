d362092862457ed37a94db016db69884e73f0a6e
sonam gupta <66966120+sonamgupta136@users.noreply.github.com>
2021-05-22 02:53:42 -0700

    Create reminder.py

---
+++ reminder.py
@@ -0,0 +1,14 @@
+import time
+from plyer import notification
+
+if __name__=="__main__":
+    while True:
+         notification.notify(
+             title="***Please Drink Water!!!",
+             message="The National Academies of Sciences ,Engineering and Medicine  determined that an adequate daily fluid intake is :About 15.5 cups (3.7 liters) of fluids for men.About 11.5 cups(2.7 liters) of fluids a day for women.",
+
+             timeout =10
+
+
+         )
+         time.sleep(60*60)
